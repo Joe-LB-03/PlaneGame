b31c9c4ea31c2f33e62778c8f5d2eeb40143050e
Louis Joseph Bishop <louis.bishop@live.co.uk>
2021-03-24 18:31:01 +0000

    aeefefefefrefff

--- shootingEnemies.py
+++ shootingEnemies.py
@@ -232,6 +232,8 @@ class GameView(arcade.View):
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     start_view = GameView()
+    window.show_view(start_view)
+    start_view.setup()
     arcade.run()
  
 if __name__ == "__main__":
